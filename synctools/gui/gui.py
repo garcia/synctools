697a952190a74af455e4102750187c05b4779304
Grant Garcia <github@grantgarcia.org>
2013-07-09 14:30:17 -0700

    Continued implementing GUI
    * Added ability to select and remove simfiles via right-click context menu
      and Delete key
    * Pressing a command's run button now opens a console and runs it

--- synctools/gui/gui.py
+++ synctools/gui/gui.py
@@ -1,17 +1,41 @@
 import codecs
 import glob
+import logging
 import os
+import pprint
 import sys
+import time
+import traceback
 import urllib
 
 import pygtk
 pygtk.require('2.0')
 import gtk
 import gtk.glade
+from simfile import Simfile
 from simfile.msd import MSDParser
 
 from synctools import commands
 from synctools.commands.all_commands import all_commands
+
+
+class GtkTextViewHandler(logging.Handler):
+    
+    def __init__(self, tv):
+        logging.Handler.__init__(self)
+        self.tv = tv
+        self.tbf = tv.get_buffer()
+        self.formatter = None
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            fs  = "%s\n"
+            self.tbf.insert(self.tbf.get_end_iter(), fs % msg)
+            self.tv.scroll_to_iter(self.tbf.get_end_iter(), 0.0, False, 0, 0)
+        except:
+            self.handleError(record)
+
 
 class SynctoolsGUI:
     
@@ -97,10 +121,82 @@ class SynctoolsGUI:
     def about_response(self, dialog, response):
         dialog.hide()
     
+    # Managing selected simfiles
+    
+    def simfile_tree_delete_selected(self):
+        simfile_tree = self.glade.get_object('simfile_tree')
+        model, pathlist = simfile_tree.get_selection().get_selected_rows()
+        # Iters need to be retrieved before altering the model, otherwise
+        # they become invalid as paths are removed
+        for iter in [model.get_iter(path) for path in pathlist]:
+            model.remove(iter)
+    
+    def simfile_tree_key_press(self, tree, event):
+        keyname = gtk.gdk.keyval_name(event.keyval)
+        if keyname == 'Delete':
+            self.simfile_tree_delete_selected()
+    
+    def simfile_tree_button_press(self, tree, event):
+        # Right click
+        if event.button == 3:
+            simfile_tree = self.glade.get_object('simfile_tree')
+            if simfile_tree.get_selection().get_selected_rows()[1]:
+                self.glade.get_object('simfile_tree_right_click').popup(
+                    None, None, None, 3, int(time.time())
+                )
+                return True
+    
+    def simfile_tree_right_click_delete(self, menuitem):
+        self.simfile_tree_delete_selected()
+    
     # Run buttons
     
-    def run_button(self, button, command):
-        print command
+    def run_button(self, button, command_name):
+        # Get command class
+        for command in all_commands:
+            if command_name == command.__name__:
+                break
+        
+        # Get option fields
+        options = {}
+        for field, widget in self.optionfields[command_name].items():
+            field_dict = [f for f in command.fields if f['name'] == field][0]
+            if field_dict['input'] == commands.FieldInputs.boolean:
+                options[field] = widget.get_active()
+            elif field_dict['input'] == commands.FieldInputs.text:
+                options[field] = widget.get_text()
+        
+        # Create output window
+        self.glade.get_object('output').show_all()
+        self.log.debug(str(options))
+        
+        # Process the simfiles
+        simfile_list = self.glade.get_object('simfiles')
+        self.log.info('Running command %r on %d simfiles.' %
+                      (command.title, len(simfile_list)))
+        try:
+            command_instance = command(options)
+        except Exception:
+            self.log.error(traceback.format_exc().splitlines()[-1])
+        for item in simfile_list:
+            self.log.info('Processing {title}...'.format(title=item[0]))
+            while gtk.events_pending():
+                gtk.main_iteration_do(False)
+            try:
+                command_instance.run(Simfile(item[-1]))
+            except Exception:
+                self.log.error(traceback.format_exc().splitlines()[-1])
+        command_instance.done()
+        self.log.info('Done.')
+        self.log.info('')
+    
+    # Output window
+    
+    def output_clear(self, button):
+        self.glade.get_object('output_textview').get_buffer().set_text('')
+    
+    def output_close(self, button):
+        self.glade.get_object('output').hide()
     
     # Initialization
     
@@ -140,8 +236,19 @@ class SynctoolsGUI:
             page.attach(run_button, 0, 2, f + 1, f + 2, 0, 0, 0, 5)
             notebook.append_page(page, gtk.Label(command.title))
         
+        # Allow selection of multiple simfiles
+        selection = self.glade.get_object('simfile_tree').get_selection()
+        selection.set_mode(gtk.SELECTION_MULTIPLE)
+        
         # Parse any file arguments
         self.add_simfiles(sys.argv[1:])
+        
+        # Set up logging
+        self.log = logging.getLogger('synctools')
+        self.log.setLevel(logging.INFO)
+        self.log.addHandler(GtkTextViewHandler(
+            self.glade.get_object('output_textview')
+        ))
         
         # Set up main window
         self.window = self.glade.get_object('synctools')
