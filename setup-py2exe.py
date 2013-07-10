8453dedf791dee704f802a7c3cabbc75810236c4
Grant Garcia <github@grantgarcia.org>
2013-07-10 00:53:59 -0700

    Import GTK runtime files for py2exe

--- setup-py2exe.py
+++ setup-py2exe.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
+from distutils.sysconfig import get_python_lib
 import os
+import shutil
 import sys
 
 import py2exe
@@ -8,6 +10,9 @@ import py2exe
 __import__('gtk')
 m = sys.modules['gtk']
 gtk_base_path = m.__path__[0]
+
+# Find zlib.pyc
+get_python_lib()
 
 # Find installed commands
 from synctools.commands.all_commands import module_names
@@ -24,6 +29,7 @@ setup(
             'packages': 'encodings',
             'includes': 'cairo, pango, pangocairo, atk, gobject, gtk, gio, wave, yaml',
             'skip_archive': True,
+            'bundle_files': 3,
         },
     },
     data_files=[
@@ -32,3 +38,6 @@ setup(
         os.path.join(gtk_base_path, '..', 'runtime', 'bin', 'libxml2-2.dll'),
     ],
 )
+
+for subdir in ('etc', 'lib', 'share'):
+    shutil.copytree(os.path.join('include', subdir), os.path.join('dist', subdir))
